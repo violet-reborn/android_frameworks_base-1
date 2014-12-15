13e0e2e8dbc5ab5bc5d9463cea5860bb23dcde19
arcardinal <andrewrcardinal@gmail.com>
2014-12-14 22:20:50 -0600

    Fix Smart Pulldown
    Currently if No dismissible or persistent is chosen, quick setting still appears when persistant notifications are present.  This fixes that.
    
    Change-Id: I168a12fcfae5149ba12f4c034bf0025413c9b6ae

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -759,8 +759,8 @@ public class NotificationPanelView extends PanelView implements
                 && shouldQuickSettingsIntercept(event.getX(), event.getY(), -1, false);
         if (mQsSmartPullDown == 1 && !mStatusBar.hasActiveClearableNotifications()
                 || mQsSmartPullDown == 2 && !mStatusBar.hasActiveVisibleNotifications()
-                || (mQsSmartPullDown == 3 && (!mStatusBar.hasActiveVisibleNotifications()
-                        || !mStatusBar.hasActiveClearableNotifications()))) {
+                || (mQsSmartPullDown == 3 && !mStatusBar.hasActiveVisibleNotifications()
+                        && !mStatusBar.hasActiveClearableNotifications())) {
             oneFingerQsOverride = true;
         }
         if ((twoFingerQsEvent || oneFingerQsOverride)
