neobuddy89/android_frameworks_base

base: add CPU info overlay [1/2]
Change-Id: Ib735656141701c638090cf5b8754b94ef11a2584
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/android/provider/Settings.java
@@ -8472,6 +8472,12 @@ public final class Settings {
         public static final String SHOW_PROCESSES = "show_processes";
 
         /**
+         * Control whether the process CPU info meter should be shown.
+         * @hide
+         */
+        public static final String SHOW_CPU = "show_cpu";
+
+        /**
          * If 1 low power mode is enabled.
          * @hide
          */

violet-reborn/android_frameworks_base-1

Change-Id: I8e2fd81edd8499eea2b1a6de8c042938387b33ef

core/java/android/provider/Settings.java
@@ -9995,6 +9995,12 @@ public final class Settings {
                 "location_settings_link_to_permissions_enabled";
 
         /**
+         * Control whether the process CPU info meter should be shown.
+         * @hide
+         */
+        public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *