omnirom/android_frameworks_base_old

SystemUI: PoorManThemes: fix aod dash animation
if notifications are dark, the background animation was dark
which didnt look right.

Change-Id: I83922299690db81047c8d7b1dac3b1bbcb0cc130

packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
@@ -576,11 +576,12 @@ public class NotificationStackScrollLayout extends ViewGroup
         // We need to manually blend in the background color.
         int scrimColor = mScrimController.getBackgroundColor();
         int awakeColor = ColorUtils.blendARGB(scrimColor, mBgColor, alpha);
+        int themeColor = getResources().getColor(R.color.notification_aod_dash_animation);
 
         // Interpolate between semi-transparent notification panel background color
         // and white AOD separator.
         float colorInterpolation = Interpolators.DECELERATE_QUINT.getInterpolation(mDarkAmount);
-        int color = ColorUtils.blendARGB(awakeColor, Color.WHITE, colorInterpolation);
+        int color = ColorUtils.blendARGB(awakeColor, themeColor, colorInterpolation);
 
         if (mCachedBackgroundColor != color) {
             mCachedBackgroundColor = color;

violet-reborn/android_frameworks_base-1

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
@@ -576,11 +576,12 @@ public class NotificationStackScrollLayout extends ViewGroup
         // We need to manually blend in the background color.
         int scrimColor = mScrimController.getBackgroundColor();
         int awakeColor = ColorUtils.blendARGB(scrimColor, mBgColor, alpha);
+        int themeColor = getResources().getColor(R.color.notification_material_background_color);
 
         // Interpolate between semi-transparent notification panel background color
         // and white AOD separator.
         float colorInterpolation = Interpolators.DECELERATE_QUINT.getInterpolation(mDarkAmount);
-        int color = ColorUtils.blendARGB(awakeColor, Color.WHITE, colorInterpolation);
+        int color = ColorUtils.blendARGB(awakeColor, themeColor, colorInterpolation);
 
         if (mCachedBackgroundColor != color) {
             mCachedBackgroundColor = color;