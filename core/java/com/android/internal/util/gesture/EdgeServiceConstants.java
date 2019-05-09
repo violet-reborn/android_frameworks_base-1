syberia-project/platform_frameworks_base

Add EdgeGesture service for system gestures [1/3]
Based on code of PieService

Change-Id: I053737e36340a5ec6814b21eb87575f9689c7e88
Signed-off-by: mydongistiny <jaysonedson@gmail.com>
Signed-off-by: DennySPB <dennyspb@gmail.com>

core/java/com/android/internal/util/gesture/EdgeServiceConstants.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project (Jens Doll)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.android.internal.util.gesture;
+
+/**
+ * Constants needed for the edge gesture service.
+ *
+ * @see com.android.internal.util.gesture.EdgeGesturePosition
+ */
+public final class EdgeServiceConstants {
+
+    private EdgeServiceConstants() {
+        // no object allowed
+    }
+
+    /**
+     * Mask for coding positions within the flags of
+     * {@code updateEdgeGestureActivationListener()}.
+     * <p>
+     * Positions are specified by {@code EdgeGesturePosition.FLAG}.
+     */
+    public static final int POSITION_MASK = 0x0000001f;
+
+    /**
+     * Mask for coding sensitivity within the flags of
+     * {@code updateEdgeGestureActivationListener()}.
+     * <p>
+     * Sensitivity influences the speed of the swipe, the trigger area, and trigger distance that
+     * is needed to activate the edge gesture.
+     */
+    public static final int SENSITIVITY_MASK = 0x70000000;
+
+    /**
+     * Number of bits to shift left, to get a integer within the {@link #SENSITIVITY_MASK}.
+     */
+    public static final int SENSITIVITY_SHIFT = 28;
+
+    /**
+     * No sensitivity specified at all, the service may choose a sensitivity level on its own.
+     */
+    public static final int SENSITIVITY_NONE = 0;
+
+    /**
+     * Default sensitivity, picked by the edge gesture service automatically.
+     */
+    public static final int SENSITIVITY_DEFAULT = 2;
+
+    /**
+     * Lowest valid sensitivity value.
+     */
+    public static final int SENSITIVITY_LOWEST = 1;
+
+    /**
+     * Highest sensitivity value.
+     */
+    public static final int SENSITIVITY_HIGHEST = 4;
+
+    /**
+     * Do not cut 10% area on th edges
+     */
+    public static final int UNRESTRICTED = 0x10;
+
+    /**
+     * This listener does not likes enabling/disabling filter
+     * because it interrupt in motion events.
+     */
+    public static final int LONG_LIVING = 0x20;
+
+}

violet-reborn/android_frameworks_base-1

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/com/android/internal/util/gesture/EdgeServiceConstants.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project (Jens Doll)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.android.internal.util.gesture;
+
+/**
+ * Constants needed for the edge gesture service.
+ *
+ * @see com.android.internal.util.gesture.EdgeGesturePosition
+ */
+public final class EdgeServiceConstants {
+
+    private EdgeServiceConstants() {
+        // no object allowed
+    }
+
+    /**
+     * Mask for coding positions within the flags of
+     * {@code updateEdgeGestureActivationListener()}.
+     * <p>
+     * Positions are specified by {@code EdgeGesturePosition.FLAG}.
+     */
+    public static final int POSITION_MASK = 0x0000001f;
+
+    /**
+     * Mask for coding sensitivity within the flags of
+     * {@code updateEdgeGestureActivationListener()}.
+     * <p>
+     * Sensitivity influences the speed of the swipe, the trigger area, and trigger distance that
+     * is needed to activate the edge gesture.
+     */
+    public static final int SENSITIVITY_MASK = 0xf0000000;
+
+    /**
+     * Number of bits to shift left, to get a integer within the {@link #SENSITIVITY_MASK}.
+     */
+    public static final int SENSITIVITY_SHIFT = 28;
+
+    /**
+     * No sensitivity specified at all, the service may choose a sensitivity level on its own.
+     */
+    public static final int SENSITIVITY_NONE = 0;
+
+    /**
+     * Default sensitivity, picked by the edge gesture service automatically.
+     */
+    public static final int SENSITIVITY_DEFAULT = 5;
+
+    /**
+     * Lowest valid sensitivity value.
+     */
+    public static final int SENSITIVITY_LOWEST = 1;
+
+    /**
+     * Highest sensitivity value.
+     */
+    public static final int SENSITIVITY_HIGHEST = 10;
+
+    /**
+     * Do not cut 10% area on th edges
+     */
+    public static final int UNRESTRICTED = 0x10;
+
+    /**
+     * This listener does not likes enabling/disabling filter
+     * because it interrupt in motion events.
+     */
+    public static final int LONG_LIVING = 0x20;
+
+}